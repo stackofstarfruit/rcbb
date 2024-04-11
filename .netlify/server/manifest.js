992a491d38b7cb37cacfd02bf29a48986bad136e
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2024-04-11 16:35:22 -0500

    Try fixing versioning again

--- .netlify/server/manifest.js
+++ .netlify/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.BsFgB8rn.js","app":"_app/immutable/entry/app.BCItHNYo.js","imports":["_app/immutable/entry/start.BsFgB8rn.js","_app/immutable/chunks/entry.DYV8MRc_.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.BCItHNYo.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.-IlM_eug.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.CAFJRtCH.js","app":"_app/immutable/entry/app.CBVPQ667.js","imports":["_app/immutable/entry/start.CAFJRtCH.js","_app/immutable/chunks/entry.Ciu-VzQm.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.CBVPQ667.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.-IlM_eug.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DSEX6WNt.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.-IlM_eug.js","_app/immutable/chunks/entry.DYV8MRc_.js"];
+export const imports = ["_app/immutable/nodes/1.CoOW6Klg.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.-IlM_eug.js","_app/immutable/chunks/entry.Ciu-VzQm.js"];
 export const stylesheets = [];
 export const fonts = [];
