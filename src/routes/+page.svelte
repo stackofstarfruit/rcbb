c00cbd371ce177581d6d33a7f3ee0349548e1f78
Kevin Kodama <kevinkodama@Kevins-MacBook-Pro.local>
2024-04-10 23:07:53 -0500

    Add some sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-blue-50 min-h-screen p-4">
+  <h1 class="text-5xl text-center text-blue-700 font-bold my-8">Rain City Book Bowl</h1>
+
+  <section class="mx-auto p-6 max-w-4xl bg-orange-100 rounded-3xl shadow-md my-4">
+    <p class="text-lg text-gray-700 text-center">Literature Trivia By & For Book Lovers</p>
+  </section>
+
+  <section class="mx-auto p-6 max-w-4xl bg-blue-100 rounded-3xl shadow-md my-4">
+    <h2 class="text-4xl text-gray-700">Upcoming Events</h2>
+    <div class="text-lg mt-2">
+      <ul>
+        <li><a href="https://www.eventbrite.com/e/861209118657?aff=oddtdtcreator" class="text-blue-600 hover:text-orange-500">4/20 RCBB @Ada's Technical Books</a></li>
+        <li><a href="https://www.eventbrite.com/e/861837859237?aff=oddtdtcreator" class="text-blue-600 hover:text-orange-500">4/21 RCBB @Charlie's Queer Books</a></li>
+        <li><a href="" class="text-blue-600 hover:text-orange-500">University of Washington (TBD)</a></li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="mx-auto p-6 max-w-4xl bg-orange-100 rounded-3xl shadow-md my-4">
+    <h2 class="text-4xl text-gray-700">Key Links</h2>
+    <div class="text-lg mt-2">
+      <a href="https://www.eventbrite.com/o/rain-city-book-bowl-80588325903" class="text-blue-600 hover:text-orange-500">EventBrite</a>
+      <a href="https://www.instagram.com/raincitybookbowl/" class="text-blue-600 hover:text-orange-500">Instagram</a>
+    </div>
+  </section>
+</div>
